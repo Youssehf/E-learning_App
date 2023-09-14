8221ff8634c8df1ad16ff862451f7d24ac214d3b
youssef <youssefsayed1352@gmail.com>
2023-09-14 15:54:13 +0300

    edited bootstrap

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,37 +1,28 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Courses = () => {
   return (
     <>
-      <div className="card text-center rounded" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <div className="row">
-            <h5 className="card-title">Secondary One</h5>
-            <p className="card-text py-5">
-              FHD videos , live sessions , online Quizzes , personal assistant
-              and high standard of thinking Question
-            </p>
-          </div>
-          <div className="row py-2">
+    <div className="contanier">
+        <div className="row">
             <div className="col">
-              <button type="button" className="btn btn-outline-primary">
-                Read More
-              </button>
+
             </div>
-          </div>
+
         </div>
-      </div>
 
       <div className="card text-center rounded" style={{ width: "18rem" }}>
         <div className="card-body">
           <div className="row">
-            <h5 className="card-title">Secondary One</h5>
-            <p className="card-text py-5">
-              FHD videos , live sessions , online Quizzes , personal assistant
-              and high standard of thinking Question
-            </p>
+            <div className="col">
+              <h5 className="card-title">Secondary One</h5>
+              <p className="card-text py-5">
+                FHD videos , live sessions , online Quizzes , personal assistant
+                and high standard of thinking Question
+              </p>
+            </div>
           </div>
+
           <div className="row py-2">
             <div className="col">
               <button type="button" className="btn btn-outline-primary">
@@ -41,16 +32,28 @@ export const Courses = () => {
           </div>
         </div>
       </div>
+    </div>
+
+    <div className="contanier">
+        <div className="row">
+            <div className="col">
+
+            </div>
+
+        </div>
 
       <div className="card text-center rounded" style={{ width: "18rem" }}>
         <div className="card-body">
           <div className="row">
-            <h5 className="card-title">Secondary One</h5>
-            <p className="card-text py-5">
-              FHD videos , live sessions , online Quizzes , personal assistant
-              and high standard of thinking Question
-            </p>
+            <div className="col">
+              <h5 className="card-title">Secondary One</h5>
+              <p className="card-text py-5">
+                FHD videos , live sessions , online Quizzes , personal assistant
+                and high standard of thinking Question
+              </p>
+            </div>
           </div>
+
           <div className="row py-2">
             <div className="col">
               <button type="button" className="btn btn-outline-primary">
@@ -60,6 +63,36 @@ export const Courses = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>    <div className="contanier">
+        <div className="row">
+            <div className="col">
+
+            </div>
+
+        </div>
+
+      <div className="card text-center rounded" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <h5 className="card-title">Secondary One</h5>
+              <p className="card-text py-5">
+                FHD videos , live sessions , online Quizzes , personal assistant
+                and high standard of thinking Question
+              </p>
+            </div>
+          </div>
+
+          <div className="row py-2">
+            <div className="col">
+              <button type="button" className="btn btn-outline-primary">
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+         </>
   );
 };
