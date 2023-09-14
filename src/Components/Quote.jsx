686d0aef8cffe01b0afc253545369c024d606cee
Yara <yaraabdelmoneam2@gmail.com>
2023-09-14 16:11:51 +0300

    qu-1

--- src/Components/Quote.jsx
+++ src/Components/Quote.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+
 export const Quote = () => {
   return (
-    <div>Quote</div>
+    <section className="Container">
+      <div className="Row">
+        <div c>
+          <img className="image-fluied" src="href=https://www.pngarts.com/explore/165713" alt="_blank" ></img>
+          <p>“Access to quality online learning is an important step in helping people future-proof their skills and seek new opportunities for growth and development.” World Economic Forum</p>
+        </div>
+      </div>
+    </section>
   )
 }
