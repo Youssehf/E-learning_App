5b711789c3a1c6a076e2d6cb79f058df7cc204bf
youssef <youssefsayed1352@gmail.com>
2023-09-14 15:04:08 +0300

    basic structure

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Layout } from '../Layout/Layout'
+import { Info } from '../Components/Info'
+import { About } from '../Components/About'
+import { Courses } from '../Components/Courses'
+import { Quote } from '../Components/Quote'
+import { Contact } from '../Components/Contact'
+
+export const Home = () => {
+  return (
+    <Layout>
+        <Info />
+        <About />
+        <Courses />
+        <Quote />
+        <Contact />
+    </Layout>
+    
+  )
+}
