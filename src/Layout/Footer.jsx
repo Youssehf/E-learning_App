1f97e19a40d7a35346711d9dcb56e1740f25a00c
mo-abdel-samie <mohamed.abdelsamie3009@gmail.com>
2023-09-14 15:49:45 +0300

    Navbar and footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
+  let dateNow = new Date();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-secondary-subtle">
+      <div className="container py-3">
+        <p className="m-0">&copy; {dateNow.getFullYear()} Mo A. Samie</p>
+      </div>
+    </footer>
+  );
+};
