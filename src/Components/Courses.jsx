4a18f1d21982f6d6edbf4244141dabac510d1d9b
youssef <youssefsayed1352@gmail.com>
2023-09-14 15:42:43 +0300

    added bootstrap

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Courses = () => {
   return (
-    <div>Courses</div>
-  )
-}
+    <>
+      <div className="card text-center rounded" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <div className="row">
+            <h5 className="card-title">Secondary One</h5>
+            <p className="card-text py-5">
+              FHD videos , live sessions , online Quizzes , personal assistant
+              and high standard of thinking Question
+            </p>
+          </div>
+          <div className="row py-2">
+            <div className="col">
+              <button type="button" className="btn btn-outline-primary">
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card text-center rounded" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <div className="row">
+            <h5 className="card-title">Secondary One</h5>
+            <p className="card-text py-5">
+              FHD videos , live sessions , online Quizzes , personal assistant
+              and high standard of thinking Question
+            </p>
+          </div>
+          <div className="row py-2">
+            <div className="col">
+              <button type="button" className="btn btn-outline-primary">
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card text-center rounded" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <div className="row">
+            <h5 className="card-title">Secondary One</h5>
+            <p className="card-text py-5">
+              FHD videos , live sessions , online Quizzes , personal assistant
+              and high standard of thinking Question
+            </p>
+          </div>
+          <div className="row py-2">
+            <div className="col">
+              <button type="button" className="btn btn-outline-primary">
+                Read More
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
